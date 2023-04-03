c75b33e2c44b8260c24d57f2f5bfaa54a1d563f0
Mirko Nasato <mirko@nasato.dev>
2023-04-03 09:00:00 +0100

    Arrays and Non-Nullability

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,11 +1,18 @@
 export const resolvers = {
   Query: {
-    job: () => {
-      return {
-        id: 'test-id',
-        title: 'The Title',
-        description: 'The description.',
-      };
+    jobs: () => {
+      return [
+        {
+          id: 'test-id-1',
+          title: 'The Title 1',
+          description: 'The description.',
+        },
+        {
+          id: 'test-id-2',
+          title: 'The Title 2',
+          description: 'The description.',
+        },
+      ];
     },
   },
 };
