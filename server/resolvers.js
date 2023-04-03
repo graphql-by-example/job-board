b0694e7ed885f816ec67e4888adf2396aedf5b79
Mirko Nasato <mirko@nasato.dev>
2023-04-03 09:00:00 +0100

    Field Resolvers

--- server/resolvers.js
+++ server/resolvers.js
@@ -4,4 +4,12 @@ export const resolvers = {
   Query: {
     jobs: () => getJobs(),
   },
+  
+  Job: {
+    date: (job) => toIsoDate(job.createdAt),
+  },
 };
+
+function toIsoDate(value) {
+  return value.slice(0, 'yyyy-mm-dd'.length);
+}
