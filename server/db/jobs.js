6720e26406a968242a3430d43898bb6347c675f4
Mirko Nasato <mirko@nasato.dev>
2023-04-04 09:00:00 +0100

    Bidirectional Associations

--- server/db/jobs.js
+++ server/db/jobs.js
@@ -7,6 +7,10 @@ export async function getJobs() {
   return await getJobTable().select();
 }
 
+export async function getJobsByCompany(companyId) {
+  return await getJobTable().select().where({ companyId });
+}
+
 export async function getJob(id) {
   return await getJobTable().first().where({ id });
 }
