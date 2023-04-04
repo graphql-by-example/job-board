1cf55501bee9cd9ecbfc0b360fbafa8e64a50941
Mirko Nasato <mirko@nasato.dev>
2023-04-04 09:00:00 +0100

    Query Variables

--- client/src/pages/JobPage.js
+++ client/src/pages/JobPage.js
@@ -1,12 +1,20 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { Link } from 'react-router-dom';
 import { formatDate } from '../lib/formatters';
-import { jobs } from '../lib/fake-data';
+import { getJob } from '../lib/graphql/queries';
 
 function JobPage() {
   const { jobId } = useParams();
+  const [job, setJob] = useState();
+  useEffect(() => {
+    getJob(jobId).then(setJob);
+  }, [jobId]);
 
-  const job = jobs.find((job) => job.id === jobId);
+  console.log('[JobPage] job:', job);
+  if (!job) {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
       <h1 className="title is-2">
