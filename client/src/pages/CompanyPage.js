2c793c0f6d30d8b8f80e4671670274a707f57688
Mirko Nasato <mirko@nasato.dev>
2023-04-04 09:00:00 +0100

    Exercise: Company by ID

--- client/src/pages/CompanyPage.js
+++ client/src/pages/CompanyPage.js
@@ -1,10 +1,18 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
-import { companies } from '../lib/fake-data';
+import { getCompany } from '../lib/graphql/queries';
 
 function CompanyPage() {
   const { companyId } = useParams();
+  const [company, setCompany] = useState();
+  useEffect(() => {
+    getCompany(companyId).then(setCompany);
+  }, [companyId]);
 
-  const company = companies.find((company) => company.id === companyId);
+  console.log('[CompanyPage] company:', company);
+  if (!company) {
+    return <div>Loading...</div>;
+  }
   return (
     <div>
       <h1 className="title">
