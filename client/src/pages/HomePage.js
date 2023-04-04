42582dce4d1ec27c379c187e2b34661ffd9611f7
Mirko Nasato <mirko@nasato.dev>
2023-04-04 09:00:00 +0100

    Component State in React

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,10 +1,14 @@
+import { useEffect, useState } from 'react';
 import JobList from '../components/JobList';
-import { jobs } from '../lib/fake-data';
 import { getJobs } from '../lib/graphql/queries';
 
-getJobs().then((jobs) => console.log('jobs:', jobs));
-
 function HomePage() {
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    getJobs().then(setJobs);
+  }, []);
+
+  console.log('[HomePage] jobs:', jobs);
   return (
     <div>
       <h1 className="title">
