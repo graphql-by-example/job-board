55669466069dfa84bf73aee2239ea24b3f2b9e28
Mirko Nasato <mirko@nasato.dev>
2023-04-07 09:00:00 +0100

    User-Company Association

--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import express from 'express';
 import { readFile } from 'node:fs/promises';
 import { authMiddleware, handleLogin } from './auth.js';
 import { resolvers } from './resolvers.js';
+import { getUser } from './db/users.js';
 
 const PORT = 9000;
 
@@ -15,8 +16,12 @@ app.post('/login', handleLogin);
 
 const typeDefs = await readFile('./schema.graphql', 'utf8');
 
-function getContext({ req }) {
-  return { auth: req.auth };
+async function getContext({ req }) {
+  if (req.auth) {
+    const user = await getUser(req.auth.sub);
+    return { user };
+  }
+  return {};
 }
 
 const apolloServer = new ApolloServer({ typeDefs, resolvers });
