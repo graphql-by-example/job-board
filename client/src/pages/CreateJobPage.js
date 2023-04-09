9e7bae2c9732cb7f14824427a83df12805cdb93a
Mirko Nasato <mirko@nasato.dev>
2023-04-09 09:00:00 +0100

    useMutation Hook

--- client/src/pages/CreateJobPage.js
+++ client/src/pages/CreateJobPage.js
@@ -1,15 +1,26 @@
+import { useMutation } from '@apollo/client';
 import { useState } from 'react';
 import { useNavigate } from 'react-router';
-import { createJob } from '../lib/graphql/queries';
+import { createJobMutation, jobByIdQuery } from '../lib/graphql/queries';
 
 function CreateJobPage() {
   const navigate = useNavigate();
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
+  const [mutate] = useMutation(createJobMutation);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const job = await createJob({ title, description });
+    const { data: { job } } = await mutate({
+      variables: { input: { title, description } },
+      update: (cache, { data }) => {
+        cache.writeQuery({
+          query: jobByIdQuery,
+          variables: { id: data.job.id },
+          data,
+        });
+      },
+    });
     console.log('job created:', job);
     navigate(`/jobs/${job.id}`);
   };
