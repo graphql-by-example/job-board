01ef546b2a9bd833d50e63426ef754a6da623213
Mirko Nasato <mirko@nasato.dev>
2023-04-09 09:00:00 +0100

    useQuery Hook

--- client/src/pages/CompanyPage.js
+++ client/src/pages/CompanyPage.js
@@ -1,35 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from '@apollo/client';
 import { useParams } from 'react-router';
 import JobList from '../components/JobList';
-import { getCompany } from '../lib/graphql/queries';
+import { companyByIdQuery } from '../lib/graphql/queries';
 
 function CompanyPage() {
   const { companyId } = useParams();
-  const [state, setState] = useState({
-    company: null,
-    loading: true,
-    error: false,
+  const { data, loading, error } = useQuery(companyByIdQuery, {
+    variables: { id: companyId },
   });
-  useEffect(() => {
-    (async () => {
-      try {
-        const company = await getCompany(companyId);
-        setState({ company, loading: false, error: false });
-      } catch (error) {
-        console.log('error:', JSON.stringify(error, null, 2));
-        setState({ company: null, loading: false, error: true });
-      }
-    })();
-  }, [companyId]);
 
-  console.log('[CompanyPage] state:', state);
-  const { company, loading, error } = state;
+  console.log('[CompanyPage]', { data, loading, error });
   if (loading) {
     return <div>Loading...</div>;
   }
   if (error) {
     return <div className="has-text-danger">Data unavailable</div>;
   }
+  const { company } = data;
   return (
     <div>
       <h1 className="title">
