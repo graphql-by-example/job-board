bfe738250f65a9ef42348748107d895509040b44
Mirko Nasato <mirko@nasato.dev>
2023-04-11 09:00:00 +0100

    Offset Clause

--- server/db/jobs.js
+++ server/db/jobs.js
@@ -3,10 +3,13 @@ import { generateId } from './ids.js';
 
 const getJobTable = () => connection.table('job');
 
-export async function getJobs(limit) {
+export async function getJobs(limit, offset) {
   const query = getJobTable().select().orderBy('createdAt', 'desc');
   if (limit) {
     query.limit(limit);
+  }
+  if (offset) {
+    query.offset(offset);
   }
   return await query;
 }
