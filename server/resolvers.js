57f459b52d008a3d8abdc2a44438809d470d50d7
Mirko Nasato <mirko@nasato.dev>
2023-10-03 09:00:00 +0100

    Object Associations

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,3 +1,4 @@
+import { getCompany } from './db/companies.js';
 import { getJobs } from './db/jobs.js';
 
 export const resolvers = {
@@ -6,6 +7,7 @@ export const resolvers = {
   },
   
   Job: {
+    company: (job) => getCompany(job.companyId),
     date: (job) => toIsoDate(job.createdAt),
   },
 };
