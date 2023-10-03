a3c757661cdbc3b03ff7dfb9d1dca6a5f17de0fa
Mirko Nasato <mirko@nasato.dev>
2023-10-03 09:00:00 +0100

    Database Access

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,18 +1,7 @@
+import { getJobs } from './db/jobs.js';
+
 export const resolvers = {
   Query: {
-    jobs: () => {
-      return [
-        {
-          id: 'test-id-1',
-          title: 'The Title 1',
-          description: 'The description.',
-        },
-        {
-          id: 'test-id-2',
-          title: 'The Title 2',
-          description: 'The description.',
-        },
-      ];
-    },
+    jobs: () => getJobs(),
   },
 };
