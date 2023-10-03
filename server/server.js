3ae895be6d26d28b191f17a9bdec3bdbdbe17845
Mirko Nasato <mirko@nasato.dev>
2023-10-03 09:00:00 +0100

    Apollo Server with Express

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware as apolloMiddleware } from '@apollo/server/express4';
 import cors from 'cors';
 import express from 'express';
+import { readFile } from 'node:fs/promises';
 import { authMiddleware, handleLogin } from './auth.js';
+import { resolvers } from './resolvers.js';
 
 const PORT = 9000;
 
@@ -9,6 +13,13 @@ app.use(cors(), express.json(), authMiddleware);
 
 app.post('/login', handleLogin);
 
+const typeDefs = await readFile('./schema.graphql', 'utf8');
+
+const apolloServer = new ApolloServer({ typeDefs, resolvers });
+await apolloServer.start();
+app.use('/graphql', apolloMiddleware(apolloServer));
+
 app.listen({ port: PORT }, () => {
   console.log(`Server running on port ${PORT}`);
+  console.log(`GraphQL endpoint: http://localhost:${PORT}/graphql`);
 });
