7547782162b6587d6556b717f5db0b243078b5b4
Mirko Nasato <mirko@nasato.dev>
2023-10-05 09:00:00 +0100

    Request States

--- client/src/pages/CompanyPage.js
+++ client/src/pages/CompanyPage.js
@@ -5,14 +5,30 @@ import { getCompany } from '../lib/graphql/queries';
 
 function CompanyPage() {
   const { companyId } = useParams();
-  const [company, setCompany] = useState();
+  const [state, setState] = useState({
+    company: null,
+    loading: true,
+    error: false,
+  });
   useEffect(() => {
-    getCompany(companyId).then(setCompany);
+    (async () => {
+      try {
+        const company = await getCompany(companyId);
+        setState({ company, loading: false, error: false });
+      } catch (error) {
+        console.log('error:', JSON.stringify(error, null, 2));
+        setState({ company: null, loading: false, error: true });
+      }
+    })();
   }, [companyId]);
 
-  console.log('[CompanyPage] company:', company);
-  if (!company) {
+  console.log('[CompanyPage] state:', state);
+  const { company, loading, error } = state;
+  if (loading) {
     return <div>Loading...</div>;
+  }
+  if (error) {
+    return <div className="has-text-danger">Data unavailable</div>;
   }
   return (
     <div>
