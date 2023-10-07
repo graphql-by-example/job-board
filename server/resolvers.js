7107b192ce3e7dc1c47d14404c53f37512fb279b
Mirko Nasato <mirko@nasato.dev>
2023-10-07 09:00:00 +0100

    Resolver Context

--- server/resolvers.js
+++ server/resolvers.js
@@ -22,7 +22,10 @@ export const resolvers = {
   },
 
   Mutation: {
-    createJob: (_root, { input: { title, description } }) => {
+    createJob: (_root, { input: { title, description } }, { auth }) => {
+      if (!auth) {
+        throw unauthorizedError('Missing authentication');
+      }
       const companyId = 'FjcJCHJALA4i'; // TODO set based on user
       return createJob({ companyId, title, description });
     },
@@ -47,6 +50,12 @@ export const resolvers = {
 function notFoundError(message) {
   return new GraphQLError(message, {
     extensions: { code: 'NOT_FOUND' },
+  });
+}
+
+function unauthorizedError(message) {
+  return new GraphQLError(message, {
+    extensions: { code: 'UNAUTHORIZED' },
   });
 }
 
