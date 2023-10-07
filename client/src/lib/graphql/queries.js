2aa774fafbb0a76b1281a762a6c7401d23a61a38
Mirko Nasato <mirko@nasato.dev>
2023-10-07 09:00:00 +0100

    Client Authentication

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -1,6 +1,15 @@
 import { GraphQLClient, gql } from 'graphql-request';
+import { getAccessToken } from '../auth';
 
-const client = new GraphQLClient('http://localhost:9000/graphql');
+const client = new GraphQLClient('http://localhost:9000/graphql', {
+  headers: () => {
+    const accessToken = getAccessToken();
+    if (accessToken) {
+      return { 'Authorization': `Bearer ${accessToken}` };
+    }
+    return {};
+  },
+});
 
 export async function createJob({ title, description }) {
   const mutation = gql`
