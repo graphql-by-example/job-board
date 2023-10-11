91e9315fcc83ce0b3b5249df51b10a7d62e0ea79
Mirko Nasato <mirko@nasato.dev>
2023-10-11 09:00:00 +0100

    Basic Pagination UI

--- client/src/lib/graphql/hooks.js
+++ client/src/lib/graphql/hooks.js
@@ -15,8 +15,9 @@ export function useJob(id) {
   return { job: data?.job, loading, error: Boolean(error) };
 }
 
-export function useJobs() {
+export function useJobs(limit, offset) {
   const { data, loading, error } = useQuery(jobsQuery, {
+    variables: { limit, offset },
     fetchPolicy: 'network-only',
   });
   return { jobs: data?.jobs, loading, error: Boolean(error) };
--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,8 +1,13 @@
+import { useState } from 'react';
 import JobList from '../components/JobList';
 import { useJobs } from '../lib/graphql/hooks';
 
+const JOBS_PER_PAGE = 5;
+
 function HomePage() {
-  const { jobs, loading, error } = useJobs();
+  const [currentPage, setCurrentPage] = useState(1);
+  const { jobs, loading, error }
+    = useJobs(JOBS_PER_PAGE, (currentPage - 1) * JOBS_PER_PAGE);
 
   console.log('[HomePage]', { jobs, loading, error });
   if (loading) {
@@ -16,6 +21,15 @@ function HomePage() {
       <h1 className="title">
         Job Board
       </h1>
+      <div>
+        <button onClick={() => setCurrentPage(currentPage - 1)}>
+          Previous
+        </button>
+        <span> {currentPage} </span>
+        <button onClick={() => setCurrentPage(currentPage + 1)}>
+          Next
+        </button>
+      </div>
       <JobList jobs={jobs} />
     </div>
   );
