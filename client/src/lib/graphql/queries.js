b2b8cb519bd5e662021abecbaeb0719127a8cf97
Mirko Nasato <mirko@nasato.dev>
2025-02-25 17:10:45 +0000

    Query Variables

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -2,6 +2,25 @@ import { GraphQLClient, gql } from 'graphql-request';
 
 const client = new GraphQLClient('http://localhost:9000/graphql');
 
+export async function getJob(id) {
+  const query = gql`
+    query JobById($id: ID!) {
+      job(id: $id) {
+        id
+        date
+        title
+        company {
+          id
+          name
+        }
+        description
+      }
+    }
+  `;
+  const { job } = await client.request(query, { id });
+  return job;
+}
+
 export async function getJobs() {
   const query = gql`
     query {
