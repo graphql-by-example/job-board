4a3869f9e86660a8e8b1658838bff9b81927b8b8
Mirko Nasato <mirko@nasato.dev>
2025-02-25 17:10:45 +0000

    Exercise: Company by ID

--- client/src/lib/graphql/queries.js
+++ client/src/lib/graphql/queries.js
@@ -2,6 +2,20 @@ import { GraphQLClient, gql } from 'graphql-request';
 
 const client = new GraphQLClient('http://localhost:9000/graphql');
 
+export async function getCompany(id) {
+  const query = gql`
+    query CompanyById($id: ID!) {
+      company(id: $id) {
+        id
+        name
+        description
+      }
+    }
+  `;
+  const { company } = await client.request(query, { id });
+  return company;
+}
+
 export async function getJob(id) {
   const query = gql`
     query JobById($id: ID!) {
