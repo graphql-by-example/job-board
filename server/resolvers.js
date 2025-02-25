4849873fa2c20602a73b03abb33b38de93606f28
Mirko Nasato <mirko@nasato.dev>
2025-02-25 17:10:45 +0000

    Exercise: Update Job

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,6 +1,6 @@
 import { GraphQLError } from 'graphql';
 import { getCompany } from './db/companies.js';
-import { createJob, deleteJob, getJob, getJobs, getJobsByCompany } from './db/jobs.js';
+import { createJob, deleteJob, getJob, getJobs, getJobsByCompany, updateJob } from './db/jobs.js';
 
 export const resolvers = {
   Query: {
@@ -28,6 +28,10 @@ export const resolvers = {
     },
 
     deleteJob: (_root, { id }) => deleteJob(id),
+
+    updateJob: (_root, { input: { id, title, description } }) => {
+      return updateJob({ id, title, description });
+    },
   },
 
   Company: {
