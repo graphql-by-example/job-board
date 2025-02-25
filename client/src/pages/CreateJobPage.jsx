8109b361b4118187eb9457e06a0ab56c279d89be
Mirko Nasato <mirko@nasato.dev>
2025-02-25 17:10:45 +0000

    useMutation Result

--- client/src/pages/CreateJobPage.jsx
+++ client/src/pages/CreateJobPage.jsx
@@ -7,7 +7,7 @@ function CreateJobPage() {
   const navigate = useNavigate();
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const [mutate] = useMutation(createJobMutation);
+  const [mutate, { loading }] = useMutation(createJobMutation);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -54,7 +54,8 @@ function CreateJobPage() {
           </div>
           <div className="field">
             <div className="control">
-              <button className="button is-link" onClick={handleSubmit}>
+              <button className="button is-link" disabled={loading}
+                onClick={handleSubmit}>
                 Submit
               </button>
             </div>
